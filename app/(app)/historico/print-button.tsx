56d9ca97b74c532bdaeb2f2f536e7d351cda25fd
movimentolivre <amzbr@protonmail.com>
2026-07-17 11:01:29 -0300

    Corrige botao de PDF que enviava o formulario sem querer

--- app/(app)/historico/print-button.tsx
+++ app/(app)/historico/print-button.tsx
@@ -3,6 +3,7 @@
 export function BotaoImprimir() {
   return (
     <button
+      type="button"
       onClick={() => window.print()}
       className="rounded-lg border border-primary px-4 py-2 text-sm font-medium text-primary print:hidden"
     >
